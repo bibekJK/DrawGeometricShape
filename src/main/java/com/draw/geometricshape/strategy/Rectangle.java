d51feb6ca2df430e406ba7fb0e393f1693b326db
unknown <bibekjang@gmail.com>
2017-06-22 22:22:44 -0400

    added draw and validate methods for Rectangle

--- src/main/java/com/draw/geometricshape/strategy/Rectangle.java
+++ src/main/java/com/draw/geometricshape/strategy/Rectangle.java
@@ -6,8 +6,49 @@ public class Rectangle implements Idraw {
 
 	@Override
 	public String draw(UserInputs userInputs) {
-		System.out.println(userInputs.getShape());
-		return null;
+		StringBuilder result = new StringBuilder();
+
+		int height = Integer.parseInt(userInputs.getHeight());
+		int breadth = Integer.parseInt(userInputs.getBreadth());
+		int row = Integer.parseInt(userInputs.getRow());
+		String text = userInputs.getText();
+		for (int i = 0; i < height; i++) {
+			if (i == row) {
+				int temp = breadth - text.length();
+				for (int k = 0; k < breadth; k++) {
+
+					if (k == temp / 2) {
+						
+						result.append(text);
+						k += text.length() - 1;
+					} else {
+						
+						result.append("*");
+					}
+				}
+			} else {
+				for (int j = 0; j < breadth; j++) {
+				
+					result.append("*");
+				}
+			}
+			
+			result.append("<br>");
+		}
+		if (validate(height, breadth, row, text) == true) {
+			return result.toString();
+		} else {
+			return "Not a valid input";
+		}
 	}
 
+	public static boolean validate(int height, int breadth, int row, String text) {
+		int len = text.length();
+		if (len > breadth || breadth < 0 || height < 0 || row>height) {
+			return false;
+		} else {
+			return true;
+
+		}
+	}
 }
