712f8dba738ede4d164728502a79a0edab4bb592
unknown <bibekjang@gmail.com>
2017-06-23 02:34:07 -0400

    test cases for Diamond

---
+++ src/test/java/com/draw/geometricshape/DiamondTest.java
@@ -0,0 +1,59 @@
+package com.draw.geometricshape;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.draw.geometricshape.domain.UserInputs;
+import com.draw.geometricshape.strategy.Diamond;
+
+public class DiamondTest {
+	Diamond diamond;
+	UserInputs userInputs;
+
+	@Before
+	public void setUp() {
+		diamond = new Diamond();
+		userInputs = new UserInputs();
+
+	}
+
+	@Test
+	public void drawMethodTestForNull() {
+		userInputs.setHeight("10");
+		userInputs.setRow("5");
+		userInputs.setText("s");
+		assertNotNull(diamond.draw(userInputs));
+	}
+
+	@Test
+	public void drawMethodTestForInvalidInputs() {
+		userInputs.setHeight("10");
+		userInputs.setRow("11");
+		userInputs.setText("s");
+		assertEquals("Not a valid input", diamond.draw(userInputs));
+
+		userInputs.setHeight("10");
+		userInputs.setRow("1");
+		userInputs.setText("test");
+		assertEquals("Not a valid input", diamond.draw(userInputs));
+	}
+
+	@Test
+	public void drawMethodTestValidInputs() {
+		userInputs.setHeight("10");
+		userInputs.setRow("5");
+		userInputs.setText("four");
+		assertEquals(206, diamond.draw(userInputs).length());
+		
+		userInputs.setHeight("4");
+		userInputs.setRow("3");
+		userInputs.setText("tw");
+				assertEquals("&nbsp;* <br>* * <br>&nbsp;* t w <br>", diamond.draw(userInputs));
+		
+
+	}
+
+}
