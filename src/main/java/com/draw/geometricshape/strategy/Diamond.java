4e6ca1780dbbf9a1cbedaaecd08bb7e6950e6cfb
unknown <bibekjang@gmail.com>
2017-06-22 22:22:01 -0400

    added draw and validate methods for Diamond

--- src/main/java/com/draw/geometricshape/strategy/Diamond.java
+++ src/main/java/com/draw/geometricshape/strategy/Diamond.java
@@ -2,13 +2,97 @@ package com.draw.geometricshape.strategy;
 
 import com.draw.geometricshape.domain.UserInputs;
 
-
 public class Diamond implements Idraw {
 
 	@Override
 	public String draw(UserInputs userInputs) {
-		System.out.println(userInputs.getShape());
-		return null;
+		StringBuilder result = new StringBuilder();
+
+		int height = Integer.parseInt(userInputs.getHeight());
+		int row = Integer.parseInt(userInputs.getRow());
+		String text = userInputs.getText();
+
+		int input = height / 2;
+
+		int spac = input - 1;
+		int min = 1;
+		int count = 1;
+		for (int i = 0; i < input; i++) {
+			for (int k = spac; k > i; k--) {
+				result.append("&nbsp;");
+			}
+
+			if (row == count) {
+				result.append("* ");
+				for (int j = 0; j < text.length(); j++) {
+					String temp = text.charAt(j) + " ";
+					result.append(temp);
+				}
+				for (int j = text.length(); j < i; j++) {
+					result.append("* ");
+
+				}
+			} else {
+				for (int j = 0; j < min; j++) {
+					result.append("* ");
+				}
+			}
+
+			min += 1;
+			result.append("<br>");
+			count++;
+		}
+		for (int m = input - 1; m > 0; m--) {
+			for (int n = spac; n >= m; n--) {
+				result.append("&nbsp;");
+			}
+			if (count == row) {
+				result.append("* ");
+				for (int q = 0; q < text.length(); q++) {
+					String temp = text.charAt(q) + " ";
+					result.append(temp);
+				}
+				for (int q = text.length(); q < m - 1; q++) {
+					result.append("* ");
+				}
+			} else {
+				for (int q = 0; q < m; q++) {
+					result.append("* ");
+				}
+			}
+
+			result.append("<br>");
+			count++;
+		}
+
+		if (validate(height, row, text) == true) {
+			return result.toString();
+		} else {
+			return "Not a valid input";
+		}
+
+	}
+
+	public static boolean validate(int height, int row, String text) {
+
+		int len = text.length();
+		if (len < 0 || row > height) {
+			return false;
+		}
+
+		if (row <= height / 2 + 1) {
+			if (len >= 0 && len <= row) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			if (len >= 0 && len <= len - row) {
+				return true;
+			} else {
+				return false;
+			}
+		}
 
 	}
 
